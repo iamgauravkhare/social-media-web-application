7717595f7c92f27ad3b97578c8675eed9e9c875a
IAmGauravKhare <iamgauravkha@gmail.com>
2024-01-08 02:25:12 +0530

    fix : initial setup

--- store/store.js
+++ store/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import rootReducer from "./Reducers";
+import rootReducer from "./reducers";
 
 export default configureStore({
   reducer: rootReducer,
